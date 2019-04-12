615528685538cb570ecd021b28c218a7e7c6732f
azxca1731 <azxca1731@gmail.com>
2019-04-12 17:51:20 +0900

    11057 complete

--- 3. 기초 다이나믹 프로그래밍/11057/main.cpp
+++ 3. 기초 다이나믹 프로그래밍/11057/main.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
 using namespace std;
-int main() {
+bool check[1001][11];
+int dp[1001][11];
+int recur(int n, int j)
+{
+    if (check[n][j])
+        return dp[n][j];
+    check[n][j] = true;
+    if (n == 1)
+    {
+        dp[n][j] = 1;
+    }
+    else
+    {
+        if (j == 0)
+        {
+            dp[n][j] = recur(n - 1, j);
+        }
+        else
+        {
+            dp[n][j] = recur(n - 1, j) + recur(n, j - 1);
+        }
+    }
+    return dp[n][j] % 10007;
+}
+int main()
+{
+    int N;
 
-
+    cin >> N;
+    int sum = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        sum += recur(N, i);
+    }
+    cout << sum % 10007 << endl;
     return 0;
 }
